aa9bb2b2809a089a43b3909818a2ca811a93d2ea
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-01-24 17:16:16 -0300

    atualização de lista de dados de projetos

--- src/data.ts
+++ src/data.ts
@@ -2,6 +2,6 @@ export const data = {
     projetosFront: [
         {id: 1, label: "Loja B7story - Front-End", link: "/projetos/b7story", img: "/assets/projetos/b7store.png"},
         {id: 2, label: "Portfolio Nextjs", link: "/projetos/portfolionext", img: "/assets/projetos/portfolio-nextjs.png"},
-        {id: 3, label: "Sistema de Despesas Pessoais", link: "/projetos/sistemadp", img: "/assets/projetos/01-b7store.png"},
+        {id: 3, label: "Sistema de Despesas Pessoais", link: "/projetos/sistemadp", img: "/assets/projetos/b7store.png"},
     ],
 }
