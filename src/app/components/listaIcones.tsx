6ee421fe015cd54be3243ad881fa623589acbfa5
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-03-02 16:56:53 -0300

    component lista de ícones recebendo icones e exibindo na home

--- src/app/components/listaIcones.tsx
+++ src/app/components/listaIcones.tsx
@@ -1,3 +1,6 @@
+import { data } from "@/data";
+import Image from "next/image";
+
 
 type ListaIconesProps = {
     id: number;
@@ -5,10 +8,20 @@ type ListaIconesProps = {
     img: string;
 }
 
-export const ListaIcones = ({}: ListaIconesProps) => {
+export const ListaIcones = () => {
     return(
-        <ul className="">
-            <li>...</li>
+        <ul className="flex flex-wrap justify-center gap-5 my-8 px-5">
+            {data.icones.map((i)=> (
+                <li key={i.id} className="text-white fill-amber-300">
+                    <Image
+                        width={40}
+                        height={40}
+                        alt={i.nomeIcon}
+                        src={i.img}
+                        className="fill-amber-400 stroke-amber-400"                        
+                    />
+                </li>
+            ))}
         </ul>
     )
 }
--- src/data.ts
+++ src/data.ts
@@ -5,6 +5,15 @@ export const data = {
         {id: 3, label: "Sistema de Despesas Pessoais", link: "/projetos/sistema-despesasp", linkProjeto: "https://dashboard-de-despesas-pessoais.vercel.app/", img: "/assets/projetos/dashboard-despesas-pessoais.png", github: "https://github.com/Marcio-Moraes/dashboard-despesas-pessoais", tech: 'Reactjs, Typescript, Styled-Components, hooks(Effect, States)', desc: "Criado para registro de despesas e receitas filtrando por mês. Foram criadas funções auxiliares para resolver problemas como: pegar o mês atual, filtrar a lista completa de dados e exibir apenas por mês, formatar datas, alterar o mês e a lista filtrada de acordo com o mês selecionado."},
     ],
     icones: [
-        {id: 1, img: "/assets/projetos/b7store.png", nomeIcon: ''}, 
+        {id: 1, img: "/assets/iconspng/reactjs.png", nomeIcon: 'ReactJS'}, 
+        {id: 2, img: "/assets/iconspng/nextjs.png", nomeIcon: 'Next.js'}, 
+        {id: 3, img: "/assets/iconspng/typescript.png", nomeIcon: 'Typescript'}, 
+        {id: 4, img: "/assets/iconspng/vite.png", nomeIcon: 'Vite'}, 
+        {id: 5, img: "/assets/iconspng/tailwindcss.png", nomeIcon: 'Tailwindcss'}, 
+        {id: 6, img: "/assets/iconspng/javascript.png", nomeIcon: 'Javascript'}, 
+        {id: 7, img: "/assets/iconspng/git.png", nomeIcon: 'Git'}, 
+        {id: 8, img: "/assets/iconspng/github.png", nomeIcon: 'Github'}, 
+        {id: 9, img: "/assets/iconspng/npm.png", nomeIcon: 'npm'}, 
+        {id: 10, img: "/assets/iconspng/figma.png", nomeIcon: 'Figma'}, 
     ],
 }
