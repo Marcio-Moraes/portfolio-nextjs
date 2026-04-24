c2721f2959f02c7f3b1a96e6a84f6e6aea88aa19
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-04-23 23:13:29 -0300

    Remoção das URLs "/contato" e "/cursos"

--- src/app/cursos/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Titulo } from "../components/layout/titulo";
-
-export default function Cursos() {
-    return (
-        <div className="py-40 bg-[#121212]">
-            <Titulo title1="Meus"  title2="Cursos" />            
-        </div>
-    );
-}
