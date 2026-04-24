ee102b0af3002a6d3fe75ede5b9c4208f7b0b1f4
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-04-23 23:11:28 -0300

    Component Título - Atualizações e novas classes como propriedade

--- src/app/components/layout/titulo.tsx
+++ src/app/components/layout/titulo.tsx
@@ -2,16 +2,17 @@ type Props = {
     title1: string | string[];
     title2: string;
     mt?: string;
+    mb?: string;
 }
 
-export const Titulo = ({title1, title2, mt}: Props) => {
+export const Titulo = ({title1, title2, mt, mb}: Props) => {
     return(
         <div className="flex flex-col items-center">
             <h1 className={`font-bold text-[36px] md:text-[72px] flex justify-center gap-3 ${mt ? " "+mt : ''}`}>
                 {title1}
                 <span className="text-amber-400">{title2}</span>
             </h1>
-            <div className="w-full max-w-xs h-0.5 mb-3 bg-amber-400"></div>
+            <div className={`w-full max-w-xs h-0.5 bg-amber-400 ${mb ? " "+mb : 'mb-3'}`}></div>
         </div>
     )
 }
