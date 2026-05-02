e55fe6705116b39dcc6d522cbd84661963faf848
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-02 12:40:32 -0300

    Nova seção Projetos

---
+++ src/app/(sections)/_projetos/page.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import { Titulo } from "../../components/layout/titulo";
+import { Projetos } from "../../components/projetos";
+
+
+export const metadata: Metadata = {
+    title: "Márcio Moraes Dev",
+    description: 'Lista de projetos desenvolvidos',
+};
+
+export default function ProjetosSection() {
+    return(
+        <div className="min-h-dvh py-30" id="projetos">
+            <Titulo title1="Lista de" title2="Projetos" mt="mt-10" />
+            <Projetos />
+        </div>
+    )
+}
