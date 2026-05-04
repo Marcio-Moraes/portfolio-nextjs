d82dd43141e82c2b259511f87ba04b547d3d9304
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-04 00:27:36 -0300

    Inserção listaCursos, e div container

--- src/app/(sections)/_cursos/page.tsx
+++ src/app/(sections)/_cursos/page.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import { Titulo } from "../../components/layout/titulo";
+import { ListaCursos } from "../../components/listaCursos";
 
 export default function CursosSection() {
     return (
         <div className="py-30 bg-[#121212]" id="cursos">
-            <Titulo title1="Meus"  title2="Cursos" />            
+            <div className="container mx-auto">
+                <Titulo title1="Meus"  title2="Cursos" />
+                <ListaCursos />
+            </div>
         </div>
     );
 }
