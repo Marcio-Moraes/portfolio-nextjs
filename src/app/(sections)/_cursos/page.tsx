6854ecfb25a246bba7a37d6981a7c20788d17453
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-04 01:19:04 -0300

    Altura da seção 100vh

--- src/app/(sections)/_cursos/page.tsx
+++ src/app/(sections)/_cursos/page.tsx
@@ -5,7 +5,7 @@ import { ListaCursos } from "../../components/listaCursos";
 
 export default function CursosSection() {
     return (
-        <div className="py-30 bg-[#121212]" id="cursos">
+        <div className="h-screen py-30 bg-[#121212]" id="cursos">
             <div className="container mx-auto">
                 <Titulo title1="Meus"  title2="Cursos" />
                 <ListaCursos />
