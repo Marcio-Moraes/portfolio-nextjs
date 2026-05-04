4766e7006b86cdf12c978c6f25376c25582af236
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-04 01:18:33 -0300

    Animação padrão para o component título com Motion

--- src/app/components/layout/titulo.tsx
+++ src/app/components/layout/titulo.tsx
@@ -1,3 +1,9 @@
+
+
+"use client"
+
+import { motion } from "motion/react";
+
 type Props = {
     title1: string | string[];
     title2: string;
@@ -8,11 +14,17 @@ type Props = {
 export const Titulo = ({title1, title2, mt, mb}: Props) => {
     return(
         <div className="flex flex-col items-center">
-            <h1 className={`font-bold text-[36px] md:text-[72px] flex justify-center gap-3 ${mt ? " "+mt : ''}`}>
-                {title1}
-                <span className="text-amber-400">{title2}</span>
-            </h1>
-            <div className={`w-full max-w-xs h-0.5 bg-amber-400 ${mb ? " "+mb : 'mb-3'}`}></div>
+            <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.8 }}
+            >
+                <h1 className={`font-bold text-[36px] md:text-[72px] flex justify-center gap-3 ${mt ? " "+mt : ''}`}>
+                    {title1}
+                    <span className="text-amber-400">{title2}</span>
+                </h1>
+                <div className={`w-full max-w-xs h-0.5 bg-amber-400 ${mb ? " "+mb : 'mb-3'}`}></div>
+            </motion.div>
         </div>
     )
 }
