3e99d2ca612d323dcb33a6581dc87acc890aa2fd
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-04 00:07:36 -0300

    Correção função "Helper" rolarPara

--- src/app/helpers/rolarPara.tsx
+++ src/app/helpers/rolarPara.tsx
@@ -1,9 +1,6 @@
 export const rolarPara = (id: string) => {
     const elemento = document.getElementById(id);
     if (elemento) {
-        elemento?.addEventListener('click', (e) => {
-            e.preventDefault();
-        });
       elemento.scrollIntoView({ behavior: 'smooth' });
     }
   };
