1bdf3904d246d38151d728792287e8457548fc7f
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-07 00:07:18 -0300

    Atualização lista cursos

--- src/app/components/listaCursos.tsx
+++ src/app/components/listaCursos.tsx
@@ -11,31 +11,56 @@ import {
 
 export function ListaCursos() {
   return (
-    <Accordion type="single" collapsible className="w-full pt-20">
+    <Accordion type="single" collapsible className="w-full pt-4">
       <AccordionItem value="item-1">
-        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Análise e Desenvolvimento de Sistemas - Descomplica Faculdade Digital "Cursando"</AccordionTrigger>
-        <AccordionContent className="pt-5 pb-20">
-          Estou no segundo período do curos de ADS, com previsão de conclusão para 2028.
+        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Fundamentos</AccordionTrigger>
+        <AccordionContent className="px-2 pt-5 pb-20">
+          <h3 className="text-xl mb-1 font-bold">B7Web Trilha Fundamentos</h3>
+          
+          <ul className="list-disc list-inside">
+            <li>HTML, CSS</li>
+            <li>JavaScript Fundamentos</li>
+            <li>JavaScript Avançado</li>
+            <li>Fundamentos de Git</li>
+            <li>GitHub na Prática</li>
+          </ul>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Fundamentos de React</AccordionTrigger>
-        <AccordionContent>
-          B7Web
+        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Front-End</AccordionTrigger>
+        <AccordionContent className="px-2 pt-5 pb-20">
+          <h3 className="text-xl mb-1 font-bold">B7Web Trilha de Cursos Front-End</h3>
+          
+          <ul className="list-disc list-inside">
+            <li>Fundamentos do Tailwind</li>
+            <li>Responsividade e Interações com Tailwind</li>
+            <li>Typescript do Zero</li>
+            <li>Fundamentos de Reactjs</li>
+            <li>Reactjs Intermediário</li>
+            <li>Tanstack Query</li>
+            <li>React Hook Form e Zod</li>
+            <li>Shadcn UI com Reactjs</li>
+            <li>Upload no Reactjs</li>
+          </ul>
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Next.js Fundamentos</AccordionTrigger>
-        <AccordionContent>
-          B7Web
+        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Back-End</AccordionTrigger>
+        <AccordionContent className="px-2 pt-5 pb-20">
+          <h3 className="text-xl mb-1 font-bold">B7Web Trilha de Cursos Back-End</h3>
+          
+          <ul className="list-disc list-inside">
+            <li>Fundamentos do Nodejs</li>
+            <li>Nodejs Avançado</li>
+            <li>Express e MongoDB</li>
+            <li>API Rest com Nodejs</li>
+            <li>Autenticação com Nodejs</li>
+            <li>Deploy com Nodejs</li>
+          </ul>
         </AccordionContent>
       </AccordionItem>
-      <AccordionItem value="item-4">
-        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">React Fundamentos</AccordionTrigger>
-        <AccordionContent>
-          B7Web
-        </AccordionContent>
-      </AccordionItem>
+      
+      
       
     </Accordion>
   )
