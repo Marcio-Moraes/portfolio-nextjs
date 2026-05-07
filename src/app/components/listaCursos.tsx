ce086a3c20ea46c70181572864d70ebf74c2359c
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-07 00:44:25 -0300

    Mobile text

--- src/app/components/listaCursos.tsx
+++ src/app/components/listaCursos.tsx
@@ -13,9 +13,9 @@ export function ListaCursos() {
   return (
     <Accordion type="single" collapsible className="w-full pt-4">
       <AccordionItem value="item-1">
-        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Fundamentos</AccordionTrigger>
+        <AccordionTrigger className="font-bold text-xl md:text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Fundamentos</AccordionTrigger>
         <AccordionContent className="px-2 pt-5 pb-20">
-          <h3 className="text-xl mb-1 font-bold">B7Web Trilha Fundamentos</h3>
+          <h3 className="text-sm md:text-xl mb-1 font-bold">B7Web Trilha Fundamentos</h3>
           
           <ul className="list-disc list-inside">
             <li>HTML, CSS</li>
@@ -27,9 +27,9 @@ export function ListaCursos() {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Front-End</AccordionTrigger>
+        <AccordionTrigger className="font-bold text-xl md:text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Front-End</AccordionTrigger>
         <AccordionContent className="px-2 pt-5 pb-20">
-          <h3 className="text-xl mb-1 font-bold">B7Web Trilha de Cursos Front-End</h3>
+          <h3 className="text-sm md:text-xl mb-1 font-bold">B7Web Trilha de Cursos Front-End</h3>
           
           <ul className="list-disc list-inside">
             <li>Fundamentos do Tailwind</li>
@@ -45,9 +45,9 @@ export function ListaCursos() {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger className="font-bold text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Back-End</AccordionTrigger>
+        <AccordionTrigger className="font-bold text-xl md:text-lg bg-amber-50 hover:bg-amber-400 no-underline text-black cursor-pointer px-2 mb-5">Trilha Back-End</AccordionTrigger>
         <AccordionContent className="px-2 pt-5 pb-20">
-          <h3 className="text-xl mb-1 font-bold">B7Web Trilha de Cursos Back-End</h3>
+          <h3 className="text-sm md:text-xl mb-1 font-bold">B7Web Trilha de Cursos Back-End</h3>
           
           <ul className="list-disc list-inside">
             <li>Fundamentos do Nodejs</li>
