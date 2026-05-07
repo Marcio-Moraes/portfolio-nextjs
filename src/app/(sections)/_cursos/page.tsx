ae6ec7627bb576ab84e4e9f73f89ea7a7a5ffb0a
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-07 00:06:21 -0300

    Altura de fixa para mínima

--- src/app/(sections)/_cursos/page.tsx
+++ src/app/(sections)/_cursos/page.tsx
@@ -3,13 +3,16 @@
 import { SubTitulo } from "@/app/components/layout/subTitulo";
 import { Titulo } from "../../components/layout/titulo";
 import { ListaCursos } from "../../components/listaCursos";
+import { ListaFormacao } from "../../components/listaFormacao";
 
 export default function EducacaoSection() {
     return (
-        <div className="h-screen py-30 bg-[#121212]" id="cursos">
+        <div className="min-h-screen py-30 bg-[#121212]" id="cursos">
             <div className="container mx-auto px-4">
                 <Titulo title1="Educação"  title2="e Cursos" />
                 <SubTitulo texto="Formação" />
+                <ListaFormacao />
+                <SubTitulo texto="Cursos" />
                 <ListaCursos />
             </div>
         </div>
