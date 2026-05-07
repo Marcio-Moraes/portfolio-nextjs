c4942c62bba2f0f3ac44983d13d15615154e437c
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-05-06 22:06:14 -0300

    Add Padding lateral para a seção Educação

--- src/app/(sections)/_cursos/page.tsx
+++ src/app/(sections)/_cursos/page.tsx
@@ -1,13 +1,15 @@
 "use client"
 
+import { SubTitulo } from "@/app/components/layout/subTitulo";
 import { Titulo } from "../../components/layout/titulo";
 import { ListaCursos } from "../../components/listaCursos";
 
 export default function EducacaoSection() {
     return (
         <div className="h-screen py-30 bg-[#121212]" id="cursos">
-            <div className="container mx-auto">
+            <div className="container mx-auto px-4">
                 <Titulo title1="Educação"  title2="e Cursos" />
+                <SubTitulo texto="Formação" />
                 <ListaCursos />
             </div>
         </div>
