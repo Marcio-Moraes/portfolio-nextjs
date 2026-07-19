552a3ea45681b8b0f9c145bb13aaf78d1f6d2ffd
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-07-18 23:24:31 -0300

    remove padding

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,14 +3,16 @@ import SobreSection from "../(sections)/_sobre/page";
 import EducacaoSection from "../(sections)/_cursos/page";
 import ContatoSection from "../(sections)/_contato/page";
 import ProjetosSection from "../(sections)/_projetos/page";
+import SkilsSection from "../(sections)/_skills/page";
 
 
 
 export default function Page() {
   return (
-   <div className="pb-40">
+   <div className="">
     <Marcio />
     <SobreSection />
+    <SkilsSection />
     <ProjetosSection />
     <EducacaoSection />
     <ContatoSection />
