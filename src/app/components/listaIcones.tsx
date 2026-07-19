a49e3cac1850198725ea5327b9e937be29a353ba
Marcio Moraes <marciomoraes.cdd@gmail.com>
2026-07-18 23:26:59 -0300

    Update style item icon

--- src/app/components/listaIcones.tsx
+++ src/app/components/listaIcones.tsx
@@ -19,10 +19,10 @@ export const ListaIcones = () => {
     }, []);
 
     return(
-        <ul className="flex flex-wrap justify-center gap-4 my-8 px-5">
+        <ul className="flex flex-wrap justify-center gap-3 my-8 px-5">
             {icones.map((i)=> (
                 <motion.li
-                    key={i.id} className={`text-white fill-amber-300 ${i.nomeIcon == 'Github' && 'bg-white '}`}
+                    key={i.id} className={`text-white bg-gray-600 p-1 rounded-md fill-amber-300`}
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.3 }}
